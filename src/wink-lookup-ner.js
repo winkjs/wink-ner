e0a9c2b9a9d4e1e92c9e21faa6348233f53d5f38
Sanjaya <sanjaya@graype.in>
2018-01-18 15:34:48 +0530

    feat(wink-lookup-ner): add wink standard base template

---
+++ src/wink-lookup-ner.js
@@ -0,0 +1,68 @@
+//     wink-lookup-ner
+//     Language agnostic named entity recognizer
+//
+//     Copyright (C) 2017-18  GRAYPE Systems Private Limited
+//
+//     This file is part of “wink-lookup-ner”.
+//
+//     “wink-lookup-ner” is free software: you can redistribute it
+//     and/or modify it under the terms of the GNU Affero
+//     General Public License as published by the Free
+//     Software Foundation, version 3 of the License.
+//
+//     “wink-lookup-ner” is distributed in the hope that it will
+//     be useful, but WITHOUT ANY WARRANTY; without even
+//     the implied warranty of MERCHANTABILITY or FITNESS
+//     FOR A PARTICULAR PURPOSE.  See the GNU Affero General
+//     Public License for more details.
+//
+//     You should have received a copy of the GNU Affero
+//     General Public License along with “wink-lookup-ner”.
+//     If not, see <http://www.gnu.org/licenses/>.
+
+//
+
+var ner = function () {
+  // Returned!
+  var methods = Object.create( null );
+  // Configuration - what tags or values to ignore during recognition phase.
+  var cfg = Object.create( null );
+
+  var defineConfig = function ( config ) {
+    cfg = config;
+
+    return cfg;
+  }; // defineConfig()
+
+  var learn = function ( ) {
+    return true;
+  }; // learn()
+
+  var recognize = function ( ) {
+    return true;
+  }; // recognize()
+
+  var exportJSON = function ( ) {
+    return true;
+  }; // exportJSON()
+
+  var importJSON = function ( ) {
+    return true;
+  }; // importJSON()
+
+  var reset = function ( ) {
+    return true;
+  }; // reset()
+
+  methods.defineConfig = defineConfig;
+  methods.learn = learn;
+  methods.predict = recognize;
+  methods.recognize = recognize;
+  methods.exportJSON = exportJSON;
+  methods.importJSON = importJSON;
+  methods.reset = reset;
+
+  return methods;
+};
+
+module.exports = ner;
