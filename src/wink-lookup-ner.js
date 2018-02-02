f863c12a7dbc3b611dae5e8ae75d8d7a0721aafc
Sanjaya <sanjaya@graype.in>
2018-02-02 21:07:51 +0530

    chore(*): rename repo as wink-ner

--- src/wink-lookup-ner.js
+++ src/wink-lookup-ner.js
@@ -1,23 +1,23 @@
-//     wink-lookup-ner
+//     wink-ner
 //     Language agnostic named entity recognizer
 //
 //     Copyright (C) 2017-18  GRAYPE Systems Private Limited
 //
-//     This file is part of “wink-lookup-ner”.
+//     This file is part of “wink-ner”.
 //
-//     “wink-lookup-ner” is free software: you can redistribute it
+//     “wink-ner” is free software: you can redistribute it
 //     and/or modify it under the terms of the GNU Affero
 //     General Public License as published by the Free
 //     Software Foundation, version 3 of the License.
 //
-//     “wink-lookup-ner” is distributed in the hope that it will
+//     “wink-ner” is distributed in the hope that it will
 //     be useful, but WITHOUT ANY WARRANTY; without even
 //     the implied warranty of MERCHANTABILITY or FITNESS
 //     FOR A PARTICULAR PURPOSE.  See the GNU Affero General
 //     Public License for more details.
 //
 //     You should have received a copy of the GNU Affero
-//     General Public License along with “wink-lookup-ner”.
+//     General Public License along with “wink-ner”.
 //     If not, see <http://www.gnu.org/licenses/>.
 
 //
