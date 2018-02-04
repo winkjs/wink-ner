0d3b520e2ab82b3f6803be82b8e1d7d7f32859c6
Sanjaya <sanjaya@graype.in>
2018-02-04 22:55:24 +0530

    test(wink-ner-specs): add test cases

--- test/wink-ner-specs.js
+++ test/wink-ner-specs.js
@@ -13,3 +13,34 @@ describe( 'instantiate ner', function () {
       expect( Object.keys( ner() ).length ).to.equal( 7 );
   } );
 } );
+
+describe( 'simple training & detection', function () {
+  var n = ner();
+  var trainingData = [
+    { text: 'raw banana', entityType: 'veg' },
+    { text: 'banana', entityType: 'fruit' },
+  ];
+  var tokens = [
+    { value: 'get', tag: 'word' },
+    { value: 'me', tag: 'word' },
+    { value: 'some', tag: 'word' },
+    { value: 'raw', tag: 'word' },
+    { value: 'bananas', tag: 'word' },
+    { value: 'bananas', tag: 'word' }
+  ];
+
+  var result = [
+    { value: 'get', tag: 'word' },
+    { value: 'me', tag: 'word' },
+    { value: 'some', tag: 'word' },
+    { entityType: 'veg', originalSeq: [ 'raw', 'bananas' ], uid: 'raw_banana', value: 'raw banana', tag: 'word' },
+    { value: 'banana', tag: 'word', originalSeq: [ 'bananas' ], uid: 'banana', entityType: 'fruit' }
+];
+  it( 'basic training with 2 entities', function () {
+      expect( n.learn( trainingData ) ).to.equal( 2 );
+  } );
+
+  it( 'detect *banana* & *raw banana* as entities', function () {
+      expect( n.recognize( tokens ) ).to.deep.equal( result );
+  } );
+} );
